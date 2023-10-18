701718b6ae50f1171894b6c100d6e7978509ed32
Roman Stupnitskyi <bamburov.r.v@gmail.com>
2023-10-18 17:56:30 +0200

    Updating init file

--- bot/routers/__init__.py
+++ bot/routers/__init__.py
@@ -1,3 +1,4 @@
 from .birthday.BirthdayRouter import birthday_router
 from .development.DevelopmentRouter import development_router
 from .general.GeneralRouter import general_router
+from .crocodile.CrocodileRouter import crocodile_router
